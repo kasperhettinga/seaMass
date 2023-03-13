b25d9e849986c0cbdd5962a3eeb0f3b0f1a15cd3
Hettinga <kasper.hettinga@wur.nl>
2023-03-13 12:37:25 +0100

    Added message
    This message helps me make a distinction between running the official biospi version or my own modified one.

--- R/seaMass.R
+++ R/seaMass.R
@@ -2,6 +2,7 @@
   packageStartupMessage(paste0("seaMass v", packageVersion("seaMass"), "  |  © 2019-2021  BIOSP", utf8::utf8_encode("\U0001f441"), "  Laboratory"))
   packageStartupMessage("This program comes with ABSOLUTELY NO WARRANTY.")
   packageStartupMessage("This is free software, and you are welcome to redistribute it under certain conditions.")
+  packageStartupMessage("This contains some minor changes by kasperhettinga")
 }
 
 
